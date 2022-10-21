b228137dac6b8989e72091747d969075c30774f8
David-WHP <woohyunpaik@gmail.com>
2022-10-21 16:25:33 -0400

    first commit

---
+++ hdl_cores/freedom/bootrom/sdboot/common.h
@@ -0,0 +1,20 @@
+//************************************************************************
+// Copyright 2021 Massachusetts Institute of Technology
+//
+// File Name:      
+// Program:        Common Evaluation Platform (CEP)
+// Description:    
+// Notes:          
+//
+//************************************************************************
+
+#ifndef _SDBOOT_COMMON_H
+#define _SDBOOT_COMMON_H
+
+#ifndef PAYLOAD_DEST
+  #define PAYLOAD_DEST MEMORY_MEM_ADDR
+#endif
+
+#define SCRATCH_DEST 0x64800000
+
+#endif
